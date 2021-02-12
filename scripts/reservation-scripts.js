209bd48169252350c97dc51824e60fbe4bd10fc7
Victoria Geykhman <geykhmanv@gmail.com>
2021-02-12 11:32:44 -0500

    timer implemented

--- scripts/reservation-scripts.js
+++ scripts/reservation-scripts.js
@@ -20,3 +20,19 @@ function validateForm() {
         alert("Please enter a valid last name!");
     }
 }//function validateForm()
+
+//countdown timer for completing the form
+let countDownDate = new Date().getTime() + 15 * 60 * 1000;
+let interval = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = "You have: " + minutes + "m" + seconds + "s to complete this form";
+
+    if(distance < 0) {
+        clearInterval(interval);
+        document.getElementById('countdown').innerHTML = "EXPIRED";
+    }
+}, 1000);
